bb896e2e90075da09145317fc6ec4603fec478bb
Sebastiaan Zeeff <33516116+SebastiaanZ@users.noreply.github.com>
2023-07-09 14:39:57 +0200

    Allow using a local config file in development
    The `Config` class will now load a `config.local.toml` if it exists
    instead of the committed `config.toml`. Since `config.local.toml` is
    ignored by git, this means that you can now set development configs
    without having to worry about accidentally changing production
    configurations.

--- EuroPythonBot/configuration.py
+++ EuroPythonBot/configuration.py
@@ -15,11 +15,14 @@ class Singleton(type):
 
 
 class Config(metaclass=Singleton):
+    _CONFIG_DEFAULT = "config.toml"
+    _CONFIG_LOCAL = "config.local.toml"
+
     def __init__(self):
         # Configuration file
         config = None
         base_path = Path(__file__).resolve().parent
-        config_path = base_path / "config.toml"
+        config_path = self._get_config_path(base_path)
         with open(config_path) as f:
             config = toml.loads(f.read())
 
@@ -56,3 +59,29 @@ class Config(metaclass=Singleton):
                 " fields."
             )
             sys.exit(-1)
+
+    def _get_config_path(self, base_path: Path) -> Path:
+        """Get the path to the relevant configuration file.
+
+        To make local development easier, the committed configuration
+        file used for production can be overridden by a local config
+        file: If a local configuration file is present, it is used
+        instead of the default configuration file.
+
+        Note that the files are not merged: All keys need to be present
+        in the local configuration file. One way of achieving this is to
+        make a copy of the committed config file and editing the value
+        you want to edit.
+
+        The local config file is added to the `.gitignore`, which means
+        is safe to create the file without having to worry about
+        accidentally committing development configurations.
+
+        :param base_path: The parent directory of the configuration file
+        :return: A path to a configuration file. Note that this path is
+          not guaranteed to exist: If the default configuration file is
+          deleted and there is no local configuration file, the path
+          points to a non-existing file
+        """
+        local_config = base_path / self._CONFIG_LOCAL
+        return local_config if local_config.is_file() else base_path / self._CONFIG_DEFAULT
