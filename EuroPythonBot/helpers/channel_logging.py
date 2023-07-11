f995420ad23abbffaed568728d207bc615076e83
kislovskiy <kislovskiy@protonmail.com>
2023-07-12 00:20:57 +0200

    just pass roles to log

--- EuroPythonBot/helpers/channel_logging.py
+++ EuroPythonBot/helpers/channel_logging.py
@@ -3,13 +3,13 @@ def display_roles(user):
     return [role.name for role in user.roles[1:]]
 
 
-async def log_to_channel(channel, interaction, error=None):
+async def log_to_channel(channel, interaction, roles=tuple(), error=None):
     user = interaction.user
     if user_name := user.nick is None:
         user_name = user.name
 
     if error is None:
-        content = f"✅ : **`{user_name}` REGISTERED**\nas {display_roles(user)}\n"
+        content = f"✅ : **`{user_name}` REGISTERED**\nas {roles}\n"
     else:
         content = f"❌ : **`{user_name}` encounter an ERROR**\n{error.__class__.__name__}: {error}\n"
 
