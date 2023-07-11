51522c4799819eaaa145ec22127be6ad78c028de
kislovskiy <kislovskiy@protonmail.com>
2023-07-12 00:26:46 +0200

    minimize the information

--- EuroPythonBot/cogs/registration.py
+++ EuroPythonBot/cogs/registration.py
@@ -66,7 +66,7 @@ class RegistrationForm(discord.ui.Modal, title="Europython 2023 Registration"):
             await interaction.user.add_roles(role)
         await log_to_channel(interaction.client.get_channel(config.REG_LOG_CHANNEL_ID), interaction, roles)
         await interaction.response.send_message(
-            f"Thank you {self.name.value}, you are now registered as {display_roles(interaction.user)}",  # noqa: E501
+            f"Thank you {self.name.value}, you are now registered.",  # noqa: E501
             ephemeral=True,
             delete_after=20,
         )
--- EuroPythonBot/helpers/channel_logging.py
+++ EuroPythonBot/helpers/channel_logging.py
@@ -1,15 +1,10 @@
-def display_roles(user):
-    """Return a list of roles of the intereaction user"""
-    return [role.name for role in user.roles[1:]]
-
-
 async def log_to_channel(channel, interaction, roles=tuple(), error=None):
     user = interaction.user
     if user_name := user.nick is None:
         user_name = user.name
 
     if error is None:
-        content = f"✅ : **`{user_name}` REGISTERED**\nas {roles}\n"
+        content = f"✅ : **`{user_name}` REGISTERED**\nas {[roles]}\n"
     else:
         content = f"❌ : **`{user_name}` encounter an ERROR**\n{error.__class__.__name__}: {error}\n"
 
