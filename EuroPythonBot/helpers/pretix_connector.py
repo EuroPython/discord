11c16367caa356bc5b0f1723489537b550382f6e
Niklas Mertsch <niklas@europython.eu>
2024-05-26 20:51:47 +0200

    Fix formatting

--- EuroPythonBot/helpers/pretix_connector.py
+++ EuroPythonBot/helpers/pretix_connector.py
@@ -21,8 +21,8 @@ _logger = logging.getLogger(f"bot.{__name__}")
 
 class PretixItem(pydantic.BaseModel):
     """Item which can be ordered, e.g. 'Business', 'Personal', 'Education'."""
-    # https://docs.pretix.eu/en/latest/api/resources/items.html
 
+    # https://docs.pretix.eu/en/latest/api/resources/items.html
     id: int
     names_by_locale: dict[str, str] = pydantic.Field(alias="name")
     variations: list[PretixItemVariation]
@@ -30,16 +30,16 @@ class PretixItem(pydantic.BaseModel):
 
 class PretixItemVariation(pydantic.BaseModel):
     """Variation of item, e.g. 'Conference', 'Tutorial', 'Volunteer'."""
-    # https://docs.pretix.eu/en/latest/api/resources/item_variations.html
 
+    # https://docs.pretix.eu/en/latest/api/resources/item_variations.html
     id: int
     names_by_locale: dict[str, str] = pydantic.Field(alias="value")
 
 
 class PretixOrder(pydantic.BaseModel):
     """Order containing one or more positions."""
-    # https://docs.pretix.eu/en/latest/api/resources/orders.html#order-resource
 
+    # https://docs.pretix.eu/en/latest/api/resources/orders.html#order-resource
     id: str = pydantic.Field(alias="code")
     status: str
     positions: list[PretixOrderPosition]
@@ -51,8 +51,8 @@ class PretixOrder(pydantic.BaseModel):
 
 class PretixOrderPosition(pydantic.BaseModel):
     """Ordered position, e.g. a ticket or a T-shirt"""
-    # https://docs.pretix.eu/en/latest/api/resources/orders.html#order-position-resource
 
+    # https://docs.pretix.eu/en/latest/api/resources/orders.html#order-position-resource
     order_id: str = pydantic.Field(alias="order")
     attendee_name: str | None
     item_id: int = pydantic.Field(alias="item")
