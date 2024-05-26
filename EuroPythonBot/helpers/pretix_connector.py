193a5150a69749685378266d330bfafddbbb157b
Niklas Mertsch <niklas@europython.eu>
2024-05-26 14:49:11 +0200

    Simplify PretixConnector

--- EuroPythonBot/helpers/pretix_connector.py
+++ EuroPythonBot/helpers/pretix_connector.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 import itertools
 import logging
 import os
-from datetime import datetime
+from datetime import datetime, timedelta
 from http import HTTPStatus
 from pathlib import Path
 from time import time
@@ -58,7 +58,7 @@ class PretixConnector(metaclass=Singleton):
         PRETIX_TOKEN = os.getenv("PRETIX_TOKEN")
         self.HEADERS = {"Authorization": f"Token {PRETIX_TOKEN}"}
 
-        self.id_to_name: dict[int, str] | None = None
+        self.item_id_to_name: dict[int, str] | None = None
         self.orders: dict[str, str | None] = {}
         self.last_fetch: datetime | None = None
 
@@ -76,12 +76,12 @@ class PretixConnector(metaclass=Singleton):
         """Fetch data from Pretix, store id_to_name mapping and formated orders internally"""
 
         _logger.info("Fetching IDs names from pretix")
-        self.id_to_name = await self._get_id_to_name_map()
+        self.item_id_to_name = await self._fetch_pretix_items()
         _logger.info("Done fetching IDs names from pretix")
 
         _logger.info("Fetching orders from pretix")
         time_start = time()
-        results_json = await self._fetch_all(f"{self.config.PRETIX_BASE_URL}/orders")
+        results_json = await self.fetch_pretix_orders(f"{self.config.PRETIX_BASE_URL}/orders")
         results = [PretixOrder(**result) for result in results_json]
         _logger.info("Fetched %r orders in%r seconds", len(results), time() - time_start)
 
@@ -89,7 +89,7 @@ class PretixConnector(metaclass=Singleton):
         for position in itertools.chain(
             *[result.positions for result in results if result.is_paid]
         ):
-            if self.id_to_name.get(position.item_id) in [
+            if self.item_id_to_name.get(position.item_id) in [
                 "T-shirt (free)",
                 "Childcare (Free)",
                 "Livestream Only",
@@ -97,12 +97,12 @@ class PretixConnector(metaclass=Singleton):
                 continue
             attendee_name = sanitize_string(position.attendee_name)
 
-            orders[f"{position.order_id}-{attendee_name}"] = self.id_to_name.get(position.item_id)
+            orders[f"{position.order_id}-{attendee_name}"] = self.item_id_to_name.get(position.item_id)
 
         self.orders = orders
         self.last_fetch = datetime.now()
 
-    async def _get_id_to_name_map(self) -> dict[int, str]:
+    async def _fetch_pretix_items(self) -> dict[int, str]:
         async with aiohttp.ClientSession(headers=self.HEADERS) as session:
             async with session.get(f"{self.config.PRETIX_BASE_URL}/items") as response:
                 if response.status != HTTPStatus.OK:
@@ -119,7 +119,7 @@ class PretixConnector(metaclass=Singleton):
 
         return id_to_name
 
-    async def _fetch_all(self, url: str):
+    async def fetch_pretix_orders(self, url: str):
         async with aiohttp.ClientSession(headers=self.HEADERS) as session:
             results = []
 
@@ -139,47 +139,23 @@ class PretixConnector(metaclass=Singleton):
         """With user input `order` and `full_name`, check for their ticket type"""
 
         key = f"{order}-{sanitize_string(input_string=full_name)}"
-        self.validate_key(key)
-        ticket_type = None
-        try:
-            ticket_type = self.orders[key]
+
+        if key in self.REGISTERED_SET:
+            raise AlreadyRegisteredError(f"Ticket already registered - id: {key}")
+
+        if key not in self.orders:
+            if datetime.now() - self.last_fetch < timedelta(minutes=15):
+                await self.fetch_data()
+
+        if key in self.orders:
             self.REGISTERED_SET.add(key)
             async with aiofiles.open(self.registered_file, mode="a") as f:
                 await f.write(f"{key}\n")
-        except KeyError:
-            async with aiohttp.ClientSession(headers=self.HEADERS) as session:
-                async with session.get(
-                    f"{self.config.PRETIX_BASE_URL}/orders",
-                    params={
-                        "code": order,
-                        "search": full_name,
-                    },
-                ) as request:
-                    if request.status == HTTPStatus.OK:
-                        data = await request.json()
-                        # when using search params, pretix returns a list of results of size 1
-                        # with a list of positions of size 1
-                        results = data.get("results")
-                        if len(results) > 0:
-                            order = PretixOrder(**results[0])
-                            if not order.is_paid:
-                                raise Exception("Order not paid")
-                            ticket_type = self.id_to_name.get(order.positions[0].item_id)
-                            self.REGISTERED_SET.add(key)
-                            async with aiofiles.open(self.registered_file, mode="a") as f:
-                                await f.write(f"{key}\n")
-                        else:
-                            raise NotFoundError(f"No ticket found - inputs: {order=}, {full_name=}")
-                    else:
-                        _logger.error("Error occurred: Status %r", request.status)
 
-        return ticket_type
+            return self.orders[key]
+
+        raise NotFoundError(f"No ticket found - inputs: {order=}, {full_name=}")
 
     async def get_roles(self, name: str, order: str) -> list[int]:
         ticket_type = await self.get_ticket_type(full_name=name, order=order)
         return self.config.TICKET_TO_ROLE.get(ticket_type)
-
-    def validate_key(self, key: str) -> bool:
-        if key in self.REGISTERED_SET:
-            raise AlreadyRegisteredError(f"Ticket already registered - id: {key}")
-        return True
