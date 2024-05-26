f971456e0b7c6780fcac558b7e32a4bf24d7fe8f
Niklas Mertsch <niklas@europython.eu>
2024-05-26 19:31:28 +0200

    Fix typo

--- EuroPythonBot/helpers/channel_logging.py
+++ EuroPythonBot/helpers/channel_logging.py
@@ -10,6 +10,6 @@ async def log_to_channel(channel, interaction, name="", order="", roles=tuple(),
         content = f"✅ : **`{user_identifier}` REGISTERED**\n{name=} {order=} {roles=}\n"
     else:
         error_name = error.__class__.__name__
-        content = f"❌ : **`{user_identifier}` encounter an ERROR**\n{error_name}: {error}\n"
+        content = f"❌ : **`{user_identifier}` encountered an ERROR**\n{error_name}: {error}\n"
 
     await channel.send(content=content)
